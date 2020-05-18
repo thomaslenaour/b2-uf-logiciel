63a79977514d0ca4f61324aecdff2829784dce5f
tlnkorr <lenaourthomas@gmail.com>
2020-05-18 13:34:20 +0200

    start invoice creation

--- app/backend/controllers/invoices.js
+++ app/backend/controllers/invoices.js
@@ -1,13 +1,32 @@
 const { validationResult } = require('express-validator')
 
-const Customer = require('../models/customer')
+const Invoice = require('../models/invoice')
 const HttpError = require('../models/http-error')
 
 const getInvoices = async (req, res, next) => {}
 
 const getInvoice = async (req, res, next) => {}
 
-const createInvoice = async (req, res, next) => {}
+const createInvoice = async (req, res, next) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return next(new HttpError('Les données saisies sont invalides', 422))
+  }
+
+  const { category, amount, isPaid } = req.body
+
+  // const createdInvoice = new Invoice({
+  //   category,
+  //   reference: `${req.userData.userId}-${new Date().getTime()}`,
+  //   amount,
+  //   invoice_pdf: invoicePdf,
+  //   is_paid: isPaid,
+  //   customer:
+  //   created_at: new Date().getTime()
+  // })
+  // res.json({ message: 'process creation' })
+}
 
 const updateInvoice = async (req, res, next) => {}
 
--- app/backend/routes/invoices.js
+++ app/backend/routes/invoices.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const { check } = require('express-validator')
 
+const checkAuth = require('../middlewares/check-auth')
 const invoicesController = require('../controllers/invoices')
 
 const router = express.Router()
 
 // /api/invoices
+router.use(checkAuth)
+
 router.get('/user/:userId', invoicesController.getInvoices)
 router.get('/:invoiceId', invoicesController.getInvoice)
 router.patch('/:invoiceId', invoicesController.updateInvoice)
 router.delete('/:invoiceId', invoicesController.deleteInvoice)
-router.post('/', invoicesController.createInvoice)
+router.post(
+  '/',
+  [
+    check('category').not().isEmpty(),
+    check('amount').isNumeric(),
+    check('isPaid').isBoolean()
+  ],
+  invoicesController.createInvoice
+)
 
 module.exports = router
