2c633acffb7b83d9259bc8c0728c908e79d085ce
tlnkorr <lenaourthomas@gmail.com>
2020-05-26 11:20:36 +0200

    clear invoices and customers

--- app/frontend/src/pages/CustomersPage.jsx
+++ app/frontend/src/pages/CustomersPage.jsx
@@ -1,12 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Pagination from '../components/Pagination'
-import AuthContext from '../context/auth'
 
 const CustomersPage = () => {
-  const auth = useContext(AuthContext)
-  auth.login(18, 'test')
-
   const [customers, setCustomers] = useState([])
   const [search, setSearch] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
